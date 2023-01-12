f11354bc1656c74636fc22cc5d87d4b71a4738d3
LORENZO <lorenzobaumgratz3@gmail.com>
2023-01-12 15:47:23 -0300

    criar cartas dinamicamente

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 let qnt=prompt("Quer jogar com quantas cartas?");
-if(qnt>=4 && qnt<=14 && qnt%2==0){
-    document.querySelector("")
+
+while(qnt<4 || qnt>14 || qnt%2!==0){
+    qnt=prompt("Quer jogar com quantas cartas?");
+    
 }
+let grade=[];
+
+
+
+
+let container=document.querySelector(".container-cartas");
+let carta=`<div onclick="selecionar(this)" class="card">
+<div class="front-face face">
+    <img src="./img/back.png" alt="">
+</div>
+<div class="back-face face">
+    <img src="./img/unicornparrot.gif" alt="">
+</div>
+</div> 
+
+`;
+
+for(let i=0;i<qnt;i++){
+    grade.push(carta);
+    container.innerHTML+=`${grade[i]}`;
+}
+
+
+/*function ativar(carta){
+    let x=carta.querySelector(".back-face");
+    x.classList.remove("back-face");
+}*/
+
