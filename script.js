042f9e1c3f7a427cd5b002cae3fba25eb4a00a46
LORENZO <lorenzobaumgratz3@gmail.com>
2023-01-13 14:07:20 -0300

    Embaralhar cartas

--- script.js
+++ script.js
@@ -2,37 +2,48 @@ let qnt=prompt("Quer jogar com quantas cartas?");
 
 while(qnt<4 || qnt>14 || qnt%2!==0){
     qnt=prompt("Quer jogar com quantas cartas?");
+}
+
+let grade=[];
+let aux=[];
+
+for(let i=1;i<=7;i++){
+    for(let j=0;j<2;j++){
+        let cartaMOD=`
+            <div onclick="selecionar(this)" class="card">
+                <div class="front-face face">
+                    <img src="./img/back.png" alt="">
+                </div>
+                <div class="back-face face">
+                    <img src="./img/carta${i}.gif" alt="">
+                </div>
+            </div> 
+
+        `;
+        aux.push(cartaMOD)
+    }
     
 }
-let grade=[];
 
-
-
-
-let container=document.querySelector(".container-cartas");
-let carta=`<div onclick="selecionar(this)" class="card">
-<div class="front-face face">
-    <img src="./img/back.png" alt="">
-</div>
-<div class="back-face face">
-    <img src="./img/unicornparrot.gif" alt="">
-</div>
-</div> 
-
-`;
-
-for(let i=0;i<qnt;i++){
-    grade.push(carta);
-    container.innerHTML+=`${grade[i]}`;
+function comparador(){
+    return Math.random() -0.5;
 }
 
+let container=document.querySelector(".container-cartas");
+
+for(let i=0;i<qnt;i++){
+    grade.push(aux[i]);
+}
+
+grade.sort(comparador);
+
+for(let i=0;i<qnt;i++){
+    container.innerHTML+=`${grade[i]}`;
+}
 
 function selecionar(carta){
     let x=carta.querySelector(".back-face");
     let y=carta.querySelector(".face");
-    x.classList.toggle("mostrar");
-    
-   
-    
+    x.classList.toggle("mostrar");  
 }
 
