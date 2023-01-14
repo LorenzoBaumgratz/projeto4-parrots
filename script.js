ce24aa8d907fdde9d02d26975a3f1823df00e74f
LORENZO <lorenzobaumgratz3@gmail.com>
2023-01-14 10:55:01 -0300

    fim de jogo + formar ou não pares

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let aux=[];
 for(let i=1;i<=7;i++){
     for(let j=0;j<2;j++){
         let cartaMOD=`
-            <div data-test="card" onclick="selecionar(this)" class="card">
+            <div data-test="card" onclick="verificar2(this)" class="card">
                 <div class="front-face face">
                     <img data-test="face-down-image" src="./img/back.png" alt="">
                 </div>
@@ -45,5 +45,43 @@ function selecionar(carta){
     let x=carta.querySelector(".back-face");
     let y=carta.querySelector(".face");
     x.classList.toggle("mostrar");  
+
+}
+
+let Selecionada1;
+let Selecionada2;
+let auxSelect=1;
+let click1;
+let fim=0;
+
+function verificar(selecionado){
+    Selecionada1=selecionado.querySelector(".back-face img").getAttribute('src');
+    auxSelect++;
+    selecionar(selecionado);
+    click1=selecionado;
+}
+
+function verificar2(x){
+    
+    if(auxSelect%2!==0){
+    verificar(x);
+    } else{
+        selecionar(x);
+        Selecionada2=x.querySelector(".back-face img").getAttribute('src');
+        auxSelect++;
+        if(Selecionada1!==Selecionada2){
+            setTimeout(selecionar,1000,click1);
+            setTimeout(selecionar,1000,x);
+        }else{
+            click1.removeAttribute("onclick");
+            x.removeAttribute("onclick");
+            fim+=2;
+            if(fim==qnt){
+                setTimeout(alert,1000,`Você ganhou em ${auxSelect-1} jogadas` );
+            }
+        }
+
+    }   
+
 }
 
